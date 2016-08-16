4dc5cbdf2a7854a32a39a44be38db801dbb25329
chenjido <chenjido@CHENJIDO3.americas.hpqcorp.net>
2016-08-15 18:00:50 -0700

    #27

--- src/exam/ZigZag.java
+++ src/exam/ZigZag.java
@@ -1,13 +1,22 @@
 package exam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ZigZag {
 
 	public static void main(String[] args) {
 		System.out.println(new ZigZag().convert("Ajjjjjjjjjjjjjjjhhhhhhhhhhhhhhhhggggggcycyctcuyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyydddd"
-				+ "dddduuuuuuuuuuuuuuuuuuuuuuuuuuggggggggggggggggggggggggggddddddddddddddeeeeeeeeeeeee", 1));
+				+ "dddduuuuuuuuuuuuuuuuuuuuuuuuuuggggggggggggggggggggggggggddddddddddddddeeeeeeeeeeeee", 4));
 		System.out.println(new ZigZag().convert("", 1));
 		
 		System.out.println(new ZigZag().reverse(0));
+		
+		for(String s: new ZigZag().letterCombinations("234")) {
+			System.out.println(s);
+		}
+		
+		new ZigZag().removeElement(new int[]{3, 2,2,3}, 3);
 	}
 
 	public String convert(String s, int numRows) {
@@ -46,4 +55,55 @@ public class ZigZag {
 		if(y>Integer.MAX_VALUE) return 0;
 		return (int)y*sign;
 	}
+	
+	public List<String> letterCombinations(String digits) {
+		List<String> result = new ArrayList<String>();
+		String[] dict = new String[]{"+", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		
+		if(digits == null || digits.equals("")) return result;
+		generateString(result, dict, digits, "", 0);
+		return result;
+    }
+    
+    public void generateString(List<String> result, String[] dict, String numbers, String s, int idx) {
+		if(idx == numbers.length()) {
+			result.add(s);
+			return;
+		}
+		int num = numbers.charAt(idx) - '0';
+		for(int i=0; i<dict[num].length(); i++) {
+			generateString(result, dict, numbers, s + dict[num].charAt(i), idx+1);
+		}
+	}
+    
+    //#27
+//    public int removeElement1(int[] nums, int val) {
+//    	int len = nums.length;
+//        if(len == 0) return 0;
+//        
+//      int i=0, j=len - 1;
+//        while(true) {
+//        	while(j>=0 && val == nums[j]) j--;
+//        	if(j<0) return 0;
+//        	
+//        	while(i<j && val != nums[i]) i++;
+//        	
+//        	if(i<j) {
+//        	nums[i] = nums[j];
+//        	} else {
+//        	    return j;
+//        	}
+//        }
+//     }
+    
+    public int removeElement(int[] nums, int val) {
+        int len = nums.length;
+        int loc = 0;
+        for(int i=0; i<len; i++) {
+            if(val != nums[i]) {
+                nums[loc++] = nums[i];
+            }
+        }
+        return loc;
+     }
 }
