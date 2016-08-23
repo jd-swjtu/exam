6da7809858a20446441507222c0e32967fef4ee9
chenjido <chenjido@CHENJIDO3.americas.hpqcorp.net>
2016-08-23 11:52:19 -0700

    #31

--- src/exam/TwoSum.java
+++ src/exam/TwoSum.java
@@ -16,6 +16,12 @@ public class TwoSum {
 		System.out.println(new TwoSum().atoi("2147483648"));*/
 
 		System.out.println(new TwoSum().atoi("9223372036854775809"));
+		
+		int[] a = new int[]{ 1, 12, 34, 56, 23, 12};//7, 2, 5, 3, 2, 1}; //{5,4,3,2,1};// {1,2,3,4,5}; //
+		new TwoSum().nextPermutation(a);
+		for(int i=0; i<a.length; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
 	}
 
 	public int[] twoSum(int[] nums, int target) {
@@ -65,24 +71,70 @@ public class TwoSum {
 	
 	//#24
 	public ListNode swapPairs(ListNode head) {
-        if(head == null) return null;
-        
-        ListNode h = new ListNode(0);
-        h.next = head;
-        
-        ListNode p = h;
-        
-        while(p.next != null && p.next.next != null) {
-        	ListNode q = p.next;
-        	ListNode qq = p.next.next;
-        	
-        	p.next = qq;
-        	q.next = qq.next;
-        	qq.next = q;
-        	
-        	p = p.next.next;
-        }
-        
-        return h.next;
-    }
+		if(head == null) return null;
+
+		ListNode h = new ListNode(0);
+		h.next = head;
+
+		ListNode p = h;
+
+		while(p.next != null && p.next.next != null) {
+			ListNode q = p.next;
+			ListNode qq = p.next.next;
+
+			p.next = qq;
+			q.next = qq.next;
+			qq.next = q;
+
+			p = p.next.next;
+		}
+
+		return h.next;
+	}
+
+	//#31 - 7 2 5 3 2 1
+	public void nextPermutation(int[] nums) {
+		boolean found = false;
+		int v = 0;
+		int i;
+		for(i = nums.length - 1; i >0; i--) {
+			if(nums[i] > nums[i-1]) {
+				found = true;
+				v = nums[i-1];
+				break;
+			}
+		}
+		
+		if(found) {
+			int j = 0;
+			int vv = Integer.MAX_VALUE;
+			for(int k = nums.length - 1; k >= i; k--) {
+				if(nums[k] > v) {
+					if(nums[k] <= vv) {
+						vv = nums[k];
+						j = k;
+					}
+				}
+			}
+			vv = nums[j];
+			nums[j] = nums[i-1];
+			nums[i-1] = vv;
+		}
+		//sort 
+		for(int j=i; j<nums.length - 1; j++) {
+			int vv = nums[j];
+			int l = 0;
+			for(int k = j+1; k<nums.length ; k++) {
+				if(nums[k] < vv) {
+					vv = nums[k];
+					l = k;
+				}
+			}
+			if(l != 0) {
+				vv = nums[j];
+				nums[j] = nums[l];
+				nums[l] = vv;
+			}
+		}
+	}
 }
