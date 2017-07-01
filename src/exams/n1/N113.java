de675460c964382ad10c97ce48ce475cd52ee088
jichen <jichen@10.0.0.2>
2017-07-01 07:46:51 -0700

    Got the answer.

--- src/exams/n1/N113.java
+++ src/exams/n1/N113.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import exams.utils.TreeNode;
 
@@ -12,8 +13,16 @@ import exams.utils.TreeNode;
  * Given a binary tree and a sum, find all root-to-leaf paths where each path's
  * sum equals the given sum.
  * 
- * For example: Given the below binary tree and sum = 22, 5 / \ 4 8 / / \ 11 13
- * 4 / \ / \ 7 2 5 1 return [ [5,4,11,2], [5,8,4,5] ]
+ * For example: Given the below binary tree and sum = 22, 
+ * 		5
+ * 	   / \
+ *    4   8
+ *   /   / \ 
+ *  11 13   4 
+ * / \     / \
+ * 7 2    5   1
+ * 
+ * return [ [5,4,11,2], [5,8,4,5] ]
  * 
  * @author jichen
  *
@@ -39,6 +48,57 @@ public class N113 {
 				results.add(tmp);
 			}
 		}
+	}
+
+	public List<List<Integer>> pathSumX(TreeNode root, int sum) {
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+		List<Integer> tmp = new ArrayList<Integer>();
+		int cSum = 0;
+
+		if (root == null || root.val > sum)
+			return results;
+
+		Stack<TreeNode> s = new Stack<>();
+		//node's level, index start from 0
+		Stack<Integer> l = new Stack<>();
+
+		int level = 0;
+		TreeNode p = root;
+		s.push(p);
+		l.push(level);
+
+		while (!s.isEmpty()) {
+			p = s.pop();
+			level = l.pop();
+
+			//Current node has not been summed, so the size of tmp should be equal to 'level'
+			while (tmp.size() > level) {
+				cSum -= tmp.remove(tmp.size() - 1);
+			}
+
+			//Use current node value, since we know add this node, the value still less than or equal to sum
+			tmp.add(p.val);
+			cSum += p.val;
+
+			if (cSum == sum && p.left == null && p.right == null) {
+				results.add(new ArrayList<>(tmp));
+				continue;
+			} 
+
+			//Before enter stack, make sure the node is meet requirement currently
+			if (p.right != null && cSum + p.right.val <= sum) {
+				l.push(level + 1);
+				s.push(p.right);
+			}
+
+			if (p.left != null && cSum + p.left.val <= sum) {
+				l.push(level + 1);
+				s.push(p.left);
+			}
+		}
+
+		return results;
 	}
 
 	public List<List<Integer>> pathSum(TreeNode root, int sum) {
@@ -95,11 +155,12 @@ public class N113 {
 
 	public static void main(String[] args) {
 		TreeNode root = TreeNode.deserialize("5,4,8,11,null,13,4,7,2,null,null,5,1");
-		System.out.println(new N113().pathSum(root, 22));
+		// System.out.println(new N113().pathSum(root, 22));
+		System.out.println(new N113().pathSumX(root, 22));
 
 		List<List<Integer>> results = new ArrayList<List<Integer>>();
 		new N113().pathSumX(root, 22, results, new ArrayList<Integer>());
-		System.out.println(results);
+		// System.out.println(results);
 	}
 
 }
