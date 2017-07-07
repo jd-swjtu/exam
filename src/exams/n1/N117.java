c5afcfebce4e811f8f3b5062b0c9873aa6c79f43
jichen <jichen@10.0.0.2>
2017-07-06 22:02:30 -0700

    update

--- src/exams/n1/N117.java
+++ src/exams/n1/N117.java
@@ -26,7 +26,7 @@ After calling your function, the tree should look like:
     4-> 5 -> 7 -> NULL
  */
 public class N117 {
-	public void connect(TreeNode root) {
+	public void connect1(TreeNode root) {
 		if (root == null)
 			return;
 		
@@ -76,12 +76,66 @@ public class N117 {
 		System.out.println("Done");
 	}
 
+	public void connect(TreeNode root) {
+		if (root == null)
+			return;
+
+		TreeNode p = null;
+		TreeNode q = root;
+		
+		TreeNode[] t = new TreeNode[3];
+
+		while(true) {
+			//q -- level has been organized ->next
+			while(q != null) {
+				int i=0;
+				//Get q children
+				if (q.left != null) t[i++] = q.left;
+				if (q.right != null) t[i++] = q.right;
+
+				//Get next child after q - q.next maybe null, or q.next no children, until at the end or find child
+				q = q.next;
+				while (q != null && (q.left == null && q.right == null)) {
+					q = q.next;
+				}
+				if (q != null) {
+					t[i++] = q.left != null?q.left:q.right;
+				}
+
+				if (i>1) {
+					t[0].next = t[1];
+				}
+				if (i>2) {
+					t[1].next = t[2];
+				}
+			
+				//No child, move to next level
+				if (i==0) {
+					break;
+				}
+
+				//Next level the first child
+				if (p == null) {
+					p = t[0];
+				}
+			}
+			
+			//p is first node of next level
+			if (p != null) {
+				q = p;
+				p = null;
+			} else
+				//next level no child now
+				break;
+        }
+	}
+
 	public static void main(String[] args) {
-		TreeNode root = TreeNode.deserialize("1,2,3,4,5,null,7,null,12,8,9,null,10");
+		//TreeNode root = TreeNode.deserialize("1,2,3,4,5,6,7,8,9,null,null,10");
+		TreeNode root = TreeNode.deserialize("20,10,30,5,15,25,35,3,8,13,17,27,29,34,38");
+		//TreeNode root = TreeNode.deserialize("1,2,3,4,5,null,7,null,12,8,9,null,10");
 		N117 n117 = new N117();
 		n117.connect(root);
 		System.out.println(root.next());
-
 	}
-
 }
